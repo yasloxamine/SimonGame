529784cfb5d5b6c24110b97b50d5cd11ba5185c5
yasloxamine <89788362+yasloxamine@users.noreply.github.com>
2024-06-26 17:01:48 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -2,17 +2,57 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+var level = 0;
+
+var started = false;
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+$(document).keypress(function (event) {
+  if (!started){
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  } 
+  console.log("A key is pressed");
+});
+
 function nextSequence() {
+    
   var randomNumber = Math.floor(Math.random() * 4);
+
   var randomChosenColour = buttonColours[randomNumber];
+
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour)
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+
+  level++;
+
+  $("#level-title").text("Level " + level);
+}
+
+function playSound(name) {
+  var audio = new Audio("./sounds/" + name + ".mp3");
   audio.play();
 }
 
-nextSequence();
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
