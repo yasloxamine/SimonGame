41c38053ad7f866e50dd4fd37e955705d10d8050
yasloxamine <89788362+yasloxamine@users.noreply.github.com>
2024-06-30 15:54:13 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -8,25 +8,28 @@ var level = 0;
 
 var started = false;
 
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+  console.log("A key is pressed");
+});
+
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
-});
 
-$(document).keypress(function (event) {
-  if (!started){
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  } 
-  console.log("A key is pressed");
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function nextSequence() {
-    
+  userClickedPattern = [];
+
   var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColour = buttonColours[randomNumber];
@@ -55,4 +58,34 @@ function animatePress(currentColour) {
   setTimeout(function () {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length == gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
